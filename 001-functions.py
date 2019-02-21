a042bd5a8b965f1f9799403bd009b59d26748c3d
Diego Salgado <dasalgadoc@gmail.com>
2019-02-21 00:19:58 -0500

    Complementando el tema de las funciones

--- 001-functions.py
+++ 001-functions.py
@@ -1,8 +1,24 @@
 def myFunction():
     return "Hello World!"
 
+def myDefaultFunction(eName, eAge = 100):
+    return "Hello, {}, you are {} years old".format(eName, eAge)
+
+
 if __name__ == "__main__":
+
+    # Parte Uno
     greeting = myFunction()
     print(greeting)
+
+    # Parte Dos
+    greetingDiego = myDefaultFunction("Diego")
+    greetingDiegoAge = myDefaultFunction("Diego","27")
+    print(greetingDiego)
+    print(greetingDiegoAge)
+
+    # Parte tres - Lambdas
+    quickGreeting = lambda eName: "Hola, {}".format(eName)
+    print(quickGreeting)
 
 
