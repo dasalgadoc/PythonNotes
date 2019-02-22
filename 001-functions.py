fe76419827cb15006d69a3b1e4f128c9d41e75e6
Diego Salgado <dasalgadoc@gmail.com>
2019-02-22 00:48:06 -0500

    Agregando más ejemplos al .py 001

--- 001-functions.py
+++ 001-functions.py
@@ -1,8 +1,33 @@
+variable_global = 200
+variable_global_2 = 300
+
 def myFunction():
     return "Hello World!"
 
+
 def myDefaultFunction(eName, eAge = 100):
     return "Hello, {}, you are {} years old".format(eName, eAge)
+
+
+def readFunction():
+    print(variable_global)
+
+
+def writeFunction():
+    global variable_global
+    variable_global = 201
+
+    return variable_global
+
+
+def writeFunction_2():
+    global variable_global
+    global variable_global_2
+
+    variable_global = 210
+    variable_global_2 = 200
+
+    return variable_global - variable_global_2
 
 
 if __name__ == "__main__":
@@ -20,5 +45,10 @@ if __name__ == "__main__":
     # Parte tres - Lambdas
     quickGreeting = lambda eName: "Hola, {}".format(eName)
     print(quickGreeting)
+
+    # Parte cuatro - Globales
+    readFunction()
+    print(writeFunction())
+    print(writeFunction_2())
 
 
