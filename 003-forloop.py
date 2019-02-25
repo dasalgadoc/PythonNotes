d4fce055a133f47d5568b2c83f69060ee2d4eb9c
Diego Salgado <dasalgadoc@gmail.com>
2019-02-24 19:18:42 -0500

    Agregar sentencias de control de ciclo

--- 003-forloop.py
+++ 003-forloop.py
@@ -15,4 +15,16 @@ if __name__ == "__main__":
     for entero in range(10):
         print(entero)
 
+    # Secuencias de ruptura
+    print("Imprimir Secuencias de Ruptura =>")
 
+    for i in range(35):
+        if i > 30 and i < 32:
+            break
+
+        # Si el número entre 1 y 35 es par
+        if i % 2 == 0:
+            print(i, end=" ")
+        else:
+            continue
+            print("Print Inalcanzable")
